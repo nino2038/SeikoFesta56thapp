0d690ce5655f57e15245ffc0d31777c2e33077e8
nino2038 <kurosame_9630@yahoo.co.jp>
2015-02-03 20:39:35 +0900

    がんばった

---
+++ SeikoFestaapp/foodCell.h
@@ -0,0 +1,13 @@
+//
+//  foodCell.h
+//  SeikoFestaapp
+//
+//  Created by 二宮啓 on 2015/02/03.
+//  Copyright (c) 2015年 SatoshiNinomiya. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface foodCell : UICollectionViewCell
+
+@end
---
+++ SeikoFestaapp/foodCell.m
@@ -0,0 +1,13 @@
+//
+//  foodCell.m
+//  SeikoFestaapp
+//
+//  Created by 二宮啓 on 2015/02/03.
+//  Copyright (c) 2015年 SatoshiNinomiya. All rights reserved.
+//
+
+#import "foodCell.h"
+
+@implementation foodCell
+
+@end
